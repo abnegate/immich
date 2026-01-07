7048212e7252320078b56971c809a0efdd5eecb1
Jake Barnby <jakeb994@gmail.com>
2026-01-08 05:29:07 +1300

    fix(server): tus path match

--- server/src/controllers/tus.controller.ts
+++ server/src/controllers/tus.controller.ts
@@ -31,7 +31,7 @@ export class TusController {
     return this.service.handleTusUpload(auth, req, res);
   }
 
-  @All(':id')
+  @All('*')
   @Authenticated({ permission: Permission.AssetUpload })
   @Endpoint({
     summary: 'Handle TUS upload request with ID',
